3c248f81acd2a6eae1f3bdf5fb01dabbc5bbf4ef
MRxRafi <rafadriveclase@gmail.com>
2020-09-15 13:08:14 +0200

    [USERTEST] Some tests added #7

--- src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
@@ -1,5 +1,37 @@
 package es.upm.miw.iwvg_devops.code;
 
-public class UserTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+public class UserTest {
+    private User user;
+
+    @BeforeEach
+    void before() {
+        List<Fraction> fractions = List.of(
+                new Fraction(0, 0),
+                new Fraction(1, 0),
+                new Fraction(1, 1)
+        );
+
+        user = new User("1", "Paco", "Martinez", fractions);
+    }
+
+    @Test
+    void testGetAttributes() {
+        assertEquals("1", user.getId());
+        assertEquals("Paco", user.getName());
+        assertEquals("Martinez", user.getFamilyName());
+
+    }
+
+    @Test
+    void testMethods() {
+        assertEquals("Paco Martinez", user.fullName());
+        assertEquals("P.", user.initials());
+    }
 }
