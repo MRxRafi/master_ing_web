5ed745e6dd955bf72f4145633aad9630a0406a07
MRxRafi <rafadriveclase@gmail.com>
2020-09-15 13:32:56 +0200

    [USERTEST] Añadidos tests

--- src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserTest {
@@ -12,11 +13,10 @@ public class UserTest {
 
     @BeforeEach
     void before() {
-        List<Fraction> fractions = List.of(
-                new Fraction(0, 0),
-                new Fraction(1, 0),
-                new Fraction(1, 1)
-        );
+        List<Fraction> fractions = new ArrayList<Fraction>();
+        fractions.add(new Fraction(0, 0));
+        fractions.add(new Fraction(1, 0));
+        fractions.add(new Fraction(1, 1));
 
         user = new User("1", "Paco", "Martinez", fractions);
     }
@@ -30,8 +30,28 @@ public class UserTest {
     }
 
     @Test
+    void testSetAttributes(){
+        String newName = "Pepe";
+        String newFamilyName = "Reina";
+
+        user.setName(newName);
+        user.setFamilyName(newFamilyName);
+
+        assertEquals(newName, user.getName());
+        assertEquals(newFamilyName, user.getFamilyName());
+    }
+
+    @Test
     void testMethods() {
         assertEquals("Paco Martinez", user.fullName());
         assertEquals("P.", user.initials());
+    }
+
+    @Test
+    void testAddFraction(){
+        Fraction newFraction = new Fraction(4, 8);
+        user.addFraction(newFraction);
+
+        assertEquals(true, user.getFractions().contains(newFraction));
     }
 }
